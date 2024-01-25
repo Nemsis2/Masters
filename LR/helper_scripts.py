Worked on mfccs always returing the same prediction. Added normalization to the data and reduced regularization options on LR which seems to have somewhat fixed the problem. Strangely normalization before dataset assembly does not seem to work only during processing.

LR/helper_scripts.py
@@ -188,6 +188,10 @@ get the EER decision threshold for the corresponding validation set
 def get_EER_threshold(y, results):
       fpr, tpr, threshold = roc_curve(y, results, pos_label=1)
       fnr = 1 - tpr
-      optimal_threshold = threshold[np.nanargmin(np.absolute((fnr - fpr)))]
+      index = np.nanargmin(np.absolute((fnr - fpr)))
+      if index == 0:
+            optimal_threshold = 1
+      else:
+            optimal_threshold = threshold[index]
       
       return optimal_threshold

LR/lr_main.py
@@ -12,6 +12,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import roc_auc_score
 from get_best_features import *
+from lr_model_scripts import *
 
 
 """
@@ -23,8 +24,8 @@ author: Michael Knight
 # declare global variables
 
 # set paths
-K_FOLD_PATH = "../data/tb/combo/multi_folds/"
-MODEL_PATH = "../models/tb/lr/"
+K_FOLD_PATH = "../../data/tb/combo/multi_folds/"
+MODEL_PATH = "../../models/tb/lr/old_melspec/"
 
 # choose which melspec we will be working on
 MELSPEC = "180_melspec_fold_"
@@ -45,7 +46,7 @@ DO_FSS = 0
 # testing options for the models
 TEST_GROUP_DECISION_FLAG = 1
 TEST_GROUP_FSS_DECISION_FLAG = 0
-VAL_MODEL_TEST_FLAG = 1
+VAL_MODEL_TEST_FLAG = 0
 
 # Hyperparameters
 BEST_C = [[10, 10, 10, 0.1],[0.01, 1, 10, 10],[1, 10, 10, 0.01]]
@@ -58,47 +59,6 @@ print("device=", device)
 if device != "cuda":
       print("exiting since cuda not enabled")
       exit(1)
-
-    
-def grid_search_lr(X, y):
-    param_grid = {
-        'C':[0.01, 0.1, 1, 10],
-        'l1_ratio':[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-    }
-
-    model = LogisticRegression(C = 0.2782559402207126, 
-    l1_ratio = 1, max_iter=1000000, 
-    solver='saga', 
-    penalty='elasticnet', 
-    multi_class = 'multinomial', 
-    n_jobs = -1)
-    clf = GridSearchCV(model, param_grid=param_grid, cv=3, verbose=True, n_jobs=-1)
-    best_clf = clf.fit(X, y)
-
-    return best_clf, clf.best_params_
-
-
-def gather_results(results, labels, names):
-    """
-    Inputs:
-    ---------
-        results: multiple model prob predictions for each value in the data with shape num_models x num_data_samples
-    
-        labels: list or array which contains a label for each value in the data
-
-        names: list or array of patient_id associated with each value in the data
-
-    Outputs:
-    --------
-        out[:,1]: averaged model prob predictions for each unique patient_id in names
-
-        out[:,2]: label associated with each value in out[:,1]
-    """
-
-    unq,ids,count = np.unique(names,return_inverse=True,return_counts=True)
-    out = np.column_stack((unq,np.bincount(ids,results[:,1])/count, np.bincount(ids,labels)/count))
-    
-    return out[:,1], out[:,2]
 
 
 def get_oracle_thresholds(results, labels, threshold):
@@ -131,7 +91,8 @@ def validate_model(folder, outer_fold):
     """
     threshold, auc = 0, 0
     for inner_fold in range(NUM_INNER_FOLDS):
-        val_data, val_labels, val_names = extract_val_data(K_FOLD_PATH + MELSPEC, outer_fold, inner_fold)
+        k_fold_path = f'../../data/tb/combo/new/180_melspec_fold_{outer_fold}.pkl'
+        val_data, val_labels, val_names = extract_dev_data(k_fold_path, inner_fold)
         X = np.array([np.mean(x, axis=0) for x in val_data])
         val_labels = val_labels.astype("int")
         model = pickle.load(open(MODEL_PATH + "val/" + folder + "/lr_" + MODEL_MELSPEC + "_outer_fold_" + str(outer_fold) + 
@@ -139,10 +100,10 @@ def validate_model(folder, outer_fold):
 
         results, val_labels = gather_results(model.predict_proba(X), val_labels, val_names) # do a forward pass through the model
         fpr, tpr, thresholds = roc_curve(val_labels, results, pos_label=1)
-        sens_threshold, spec_threshold = get_oracle_thresholds(results, val_labels, thresholds)
-        threshold += sens_threshold
+        #sens_threshold, spec_threshold = get_oracle_thresholds(results, val_labels, thresholds)
+        #threshold += sens_threshold
         # get EER threshold
-        # threshold +=  get_optimal_threshold(val_labels, results)
+        threshold += get_EER_threshold(val_labels, results)
 
         auc += roc_auc_score(val_labels, results)
 
@@ -172,8 +133,9 @@ def test_model(folder, outer, optimal_threshold):
     for inner in range(NUM_INNER_FOLDS):
         models.append(pickle.load(open(MODEL_PATH + "GD/" + folder + "/lr_" + MODEL_MELSPEC + "_outer_fold_" + str(outer) + 
                                         "_inner_fold_" + str(inner), 'rb'))) # load in the model
-               
-    data, labels, names = extract_test_data(K_FOLD_PATH + TEST, outer)
+    
+    k_fold_path = f'../../data/tb/combo/new/test/test_dataset_melspec_180_fold_{outer}.pkl' 
+    data, labels, names = extract_test_data(k_fold_path)
     X = np.array([np.mean(x, axis=0) for x in data])
     labels = labels.astype("int")
 
@@ -264,7 +226,7 @@ def main():
                 train_data = np.array([np.mean(x, axis=0) for x in train_data])
                 train_labels = train_labels.astype("int")
                 
-                val_data, val_labels, val_names = extract_val_data(K_FOLD_PATH + MELSPEC, outer, inner)
+                val_data, val_labels, val_names = extract_dev_data(K_FOLD_PATH + MELSPEC, outer, inner)
                 val_data = np.array([np.mean(x, axis=0) for x in val_data])
                 val_labels = val_labels.astype("int")
 

LR/lr_model_scripts.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 import pickle
+from sklearn.metrics import roc_auc_score
 
 # custom scripts
 from data_grab import extract_dev_data
@@ -25,24 +26,21 @@ def grid_search_lr(X, y):
 
         best_clf.params: Optimal hyperparameters determined by the GridSearch
     """
-    X = np.array([np.mean(x, axis=0) for x in X])
-    y = y.astype("int")
     
     param_grid = {
-        'C':[0.01, 0.1, 1, 10],
+        'C':[ 1, 10],
         'l1_ratio':[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
     }
-
     model = LogisticRegression(C = 0.2782559402207126, 
     l1_ratio = 1, max_iter=1000000, 
     solver='saga', 
     penalty='elasticnet', 
     multi_class = 'multinomial', 
     n_jobs = -1,
-    tol=0.001)
+    tol=0.0001)
     clf = GridSearchCV(model, param_grid=param_grid, cv=3, verbose=True, n_jobs=-1)
     best_clf = clf.fit(X, y)
-
+    print(best_clf.best_params_)
     return best_clf, best_clf.best_params_
 
 
@@ -87,9 +85,9 @@ def get_decision_threshold(feature_type, n_feature, n_outer, n_inner):
 
     Outputs:
     --------
-    avg_threshold: (float64) The average EER decision threshold over all models in the k-fold cross validation
+    outer_threshold: (list) The average EER decision threshold over all inner models in each outer fold. Has length = num outer folds
     """
-    avg_threshold = 0
+    outer_threshold = []
     for outer in range(n_outer): 
         outer_avg_threshold = 0
         for inner in range(n_inner):
@@ -106,6 +104,25 @@ def get_decision_threshold(feature_type, n_feature, n_outer, n_inner):
             # make predictions and calculate the threshold based off the EER
             results, val_labels = gather_results(dev_model.predict_proba(X), labels, names) # do a forward pass through the model
             outer_avg_threshold += get_EER_threshold(val_labels, results)
-        avg_threshold += outer_avg_threshold/n_inner
+        outer_threshold.append(outer_avg_threshold/n_inner)
 
-    return avg_threshold/n_outer
+    return outer_threshold
+
+
+def labels_per_frame(data, labels):
+    """
+    Inputs:
+    ---------
+        data: np.array which contains melspec samples of each cough
+    
+        labels: list or array which contains a label for each value in the data np.array
+
+    Outputs:
+    --------
+        per_frame_label: np.array which contains a label for each frame 
+    """
+    per_frame_label = []
+    for i in range(len(labels)):
+        for j in range(data[i].shape[0]):
+            per_frame_label.append(labels[i])
+    return np.array(per_frame_label)

LR/train_lr.py
@@ -49,10 +49,21 @@ def create_inner_lr(feature_type, n_feature, model_type='dev'):
                     print("Inner fold=", inner)
 
                     k_fold_path = f'../../data/tb/combo/new/{n_feature}_{feature_type}_fold_{outer}.pkl'
+                    print(k_fold_path)
                     if model_type == 'dev':
                         data, labels = extract_inner_fold_data(k_fold_path, inner)
                     elif model_type =='em':
                         data, labels = extract_outer_fold_data(k_fold_path)
+
+                    print(data[0].shape)
+                   
+                    # for by frame
+                    #labels = labels_per_frame(data, labels)
+                    #data = np.vstack(data)
+
+                    # for averaging
+                    data = np.array([np.mean(x, axis=0) for x in data])
+                    labels = labels.astype("int")
 
                     model, params = grid_search_lr(data, labels)
                     pickle.dump(model, open(f'{model_path}lr_{feature_type}_{n_feature}_outer_fold_{outer}_inner_fold_{inner}', 'wb')) # save the model
@@ -64,7 +75,7 @@ def create_inner_lr(feature_type, n_feature, model_type='dev'):
 def main():
     for feature_type in ['mfcc', 'melspec', 'lfb']:
         if feature_type == 'mfcc':
-            features = [13,26,39]
+            features = [13, 26, 39]
         elif feature_type == 'melspec' or feature_type == 'lfb':
             features = [80, 128, 180] 
         

LR/train_mfcc.py
@@ -1,5 +1,6 @@
 # libraries
 import torch as th
+import os
 import pickle
 
 # custom scripts
@@ -22,6 +23,59 @@ print("device=", device)
 if device != "cuda":
       print("exiting since cuda not enabled")
       exit(1)
+
+def normalize(mfcc):
+    mfcc = (mfcc-np.max(mfcc))/(np.max(mfcc)-np.min(mfcc))
+    return mfcc
+
+
+
+def create_inner_lr(feature_type, n_feature, model_type='dev'):
+    """
+    Description:
+    ---------
+    
+    Inputs:
+    ---------
+
+    Outputs:
+    --------
+
+    """
+    model_path = f'../../models/tb/lr/{feature_type}/{n_feature}_{feature_type}/{model_type}/'
+    
+    if len(os.listdir(model_path)) == 0: # if the folder is empty
+        print(f'Creating {model_type} models for {n_feature}_{feature_type}')
+
+        for outer in range(NUM_OUTER_FOLDS):
+                print("Outer fold=", outer)
+                
+                for inner in range(NUM_INNER_FOLDS):
+                    print("Inner fold=", inner)
+
+                    k_fold_path = f'../../data/tb/combo/new/{n_feature}_{feature_type}_fold_{outer}.pkl'
+                    print(k_fold_path)
+                    if model_type == 'dev':
+                        data, labels = extract_inner_fold_data(k_fold_path, inner)
+                    elif model_type =='em':
+                        data, labels = extract_outer_fold_data(k_fold_path)
+
+                    for i in range(data.shape[0]):
+                       for j in range(data[i].shape[0]):
+                            if np.all(data[i][j]) != 0:
+                                data[i][j] = normalize(data[i][j])
+
+
+
+                    # for averaging
+                    data = np.array([np.mean(x, axis=0) for x in data])
+                    labels = labels.astype("int")
+
+                    model, params = grid_search_lr(data, labels)
+                    pickle.dump(model, open(f'{model_path}lr_{feature_type}_{n_feature}_outer_fold_{outer}_inner_fold_{inner}', 'wb')) # save the model
+    
+    else:
+        print(f'Models already exist for type:{model_type}_{n_feature}_{feature_type}. Skipping...')
 
 
 
@@ -51,11 +105,12 @@ def test_lr(feature_type, n_feature, threshold, model_type='em'):
         # grab the testing data
         k_fold_path = f'../../data/tb/combo/new/test/test_dataset_{feature_type}_{n_feature}_fold_{outer}.pkl' 
         data, labels, names = extract_test_data(k_fold_path)
-        
-        # for by frame
-        #labels = labels_per_frame(data, labels)
-        #names = labels_per_frame(data, names) # misusing this function to keep num_names = num_labels
-        #X = np.vstack(data)
+
+        for i in range(data.shape[0]):
+            for j in range(data[i].shape[0]):
+                if np.all(data[i][j]) != 0:
+                    data[i][j] = normalize(data[i][j])
+
         
         # for averaging
         X = np.array([np.mean(x, axis=0) for x in data])
@@ -90,13 +145,14 @@ def test_lr(feature_type, n_feature, threshold, model_type='em'):
 
 
 def main():
-    for feature_type in ['mfcc', 'melspec', 'lfb']:
-        if feature_type == 'mfcc':
-            features = [13, 26, 39]
-        elif feature_type == 'melspec' or feature_type == 'lfb':
-            features = [80, 128, 180] 
-        
-        for n_feature in features:
+    for feature_type in ['mfcc']:    
+        for n_feature in  [13, 26, 39]:
+            #create models for development (threshold calculation)
+            create_inner_lr(feature_type, n_feature,'dev')
+            
+            # create models for ensemble testing
+            create_inner_lr(feature_type, n_feature,'em')
+
             # get the optimal threshold based off the EER
             threshold = get_decision_threshold(feature_type, n_feature, NUM_OUTER_FOLDS, NUM_INNER_FOLDS)
 
@@ -106,6 +162,7 @@ def main():
             print(f'AUC for {n_feature}_{feature_type}: {auc}')
             print(f'Sens for {n_feature}_{feature_type}: {sens}')
             print(f'Spec for {n_feature}_{feature_type}: {spec}')
+
 
 if __name__ == "__main__":
     main()